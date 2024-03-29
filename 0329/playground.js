5fe81a33506e638472bba5a95f816599c870412d
John Liu <johnliutw@hotmail.com>
2024-03-29 14:49:38 +0800

    Day2 - 小試身手4

--- 0329/playground.js
+++ 0329/playground.js
@@ -1,3 +1,22 @@
+// 小試身手4
+// 1. 標準方式
+// function getRealPrice(price, discount = 0.2){
+//   return price * (1 - discount);
+// }
+
+// 2. 函式表達式
+// const getRealPrice = function(price, discount = 0.2){
+//   return price * (1 - discount);
+// }
+
+// 3. 箭頭函式
+const getRealPrice = (price, discount = 0.2) => price * (1 - discount);
+
+console.log(getRealPrice(100));
+console.log(getRealPrice(100, 0.3));
+
+
+
 // 小試身手3
 function findMax(...numbers){
   let result = 0;
