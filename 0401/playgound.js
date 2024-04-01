63adb0c041b9644040f8adc3ffe0b80d47a63e3a
John Liu <johnliutw@hotmail.com>
2024-04-01 09:19:55 +0800

    Day2 - 回家作業

---
+++ 0401/playgound.js
@@ -0,0 +1,45 @@
+
+
+// Day2 回家作業
+let person = {
+    name: '野原新之助',
+    age: 30,
+    hobbies: ['drawing', 'coding', 'reading', 'swimming', 'skating', 'running'],
+    getInfo: function(){
+      return `你好, 我是${this.name}, 今年${this.age}歲`
+    },
+    isAdult: function(){
+      return this.age > 18 ? true : false;
+    },
+    printPerson: function(){
+      for( let key in person ){
+        if(typeof person[key] === 'function'){
+           continue;
+        }
+        
+        if(key === 'hobbies'){
+          let count = 1;
+          for(let hobby of person['hobbies']){
+            if(hobby === 'coding'){
+               continue;
+            }
+            
+            console.log(`hobby: ${hobby}`);
+            count ++;
+            if(count === 5){
+               break;
+            }
+          }
+        } else {
+          console.log(`${key}: ${person[key]}`); 
+        }
+      }
+    }
+  }
+  
+  console.log(person.getInfo());
+  console.log(person.isAdult());
+  person.printPerson();
+  
+  
+  
