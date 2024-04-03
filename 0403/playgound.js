8495d1000a792e917baaf1226643141bf0e65492
John Liu <johnliutw@hotmail.com>
2024-04-03 16:47:55 +0800

    Day5 - 回家作業

--- 0403/playgound.js
+++ 0403/playgound.js
@@ -1,3 +1,45 @@
+// 回家作業練習
+function isMobilePhone(number) {
+  if (number.length !== 10) {
+    return false;
+  }
+  for(let i = 0; i < number.length; i++) {
+    if (isNaN(parseInt(number[i]))) {
+      return false;
+    }
+  }
+  return true;
+}
+console.log('1234567890', isMobilePhone('1234567890'));
+console.log('123456789', isMobilePhone('123456789'));
+console.log('12345678901', isMobilePhone('12345678901'));
+console.log('12345678A0', isMobilePhone('12345678A0'));
+
+function isCreditCard(number, validMonth, validYear) {
+  const currentYear = new Date().getFullYear();
+  const currentMonth = new Date().getMonth() + 1;
+  
+  if (number.length !== 16 || isNaN(parseInt(number))) {
+    return false;
+  }
+  
+  if (validMonth < 1 || validMonth > 12) {
+    return false;
+  }
+  
+  if (validYear < currentYear || (validYear === currentYear && validMonth < currentMonth)) {
+    return false;
+  }
+  
+  return true;
+}
+
+console.log('1234567812345678', 12, 2025, isCreditCard('1234567812345678', 12, 2025));
+console.log('1234567812345678', 12, 2023, isCreditCard('1234567812345678', 12, 2023));
+console.log('1234567812345678', 13, 2025, isCreditCard('1234567812345678', 13, 2025));
+console.log('12345678O123456', 12, 2026, isCreditCard('12345678O123456', 12, 2026));
+
+
 //小試身手1
 let data = '{"name": "John Doe", "age": 30, "city": "New York"}';
 
